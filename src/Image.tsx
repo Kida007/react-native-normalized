c6c4d27b1220dded2e3d5163f87b5f2ddcbdc95c
Jonny Burger <jonathanburger11@gmail.com>
2019-08-16 14:08:25 +0200

    copy over static methods from image

--- src/Image.tsx
+++ src/Image.tsx
@@ -1,8 +1,14 @@
 import React, { forwardRef } from "react";
 import { Image as NativeImage, ImageProps } from "react-native";
 
-const Image = forwardRef((props: ImageProps, ref: React.Ref<any>) => {
+const Image: any = forwardRef((props: ImageProps, ref: React.Ref<any>) => {
   return <NativeImage {...(ref ? { ref } : {})} fadeDuration={0} {...props} />;
 });
+
+Image.prefetch = NativeImage.prefetch;
+Image.abortPrefetch = NativeImage.abortPrefetch;
+Image.getSize = NativeImage.getSize;
+Image.queryCache = NativeImage.queryCache;
+Image.resolveAssetSource = NativeImage.resolveAssetSource;
 
 export default Image;
