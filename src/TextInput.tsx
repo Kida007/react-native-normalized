e0041d10b34a96b375f1227e9fdad48dc99904df
Jonny Burger <jonathanburger11@gmail.com>
2020-03-10 22:45:13 +0100

    normalize

--- src/TextInput.tsx
+++ src/TextInput.tsx
@@ -1,15 +1,25 @@
 import React, { forwardRef } from "react";
-import { TextInput as NativeTextInput, TextInputProps } from "react-native";
+import {
+  TextInput as NativeTextInput,
+  TextInputProps,
+  StyleSheet
+} from "react-native";
+
+const styles = StyleSheet.create({
+  black: {
+    color: "black"
+  }
+});
 
 const TextInput = forwardRef((props: TextInputProps, ref: React.Ref<any>) => {
   const newProps = props.multiline ? { textAlignVertical: "top" as "top" } : {};
-  const { style = { color: "#000" }, ...otherProps } = props;
+  const { style, ...otherProps } = props;
   return (
     <NativeTextInput
       {...(ref ? { ref } : {})}
       underlineColorAndroid="transparent"
       placeholderTextColor="#C7C7CD"
-      style={style}
+      style={[styles.black, style].filter(Boolean)}
       {...newProps}
       {...otherProps}
     />
