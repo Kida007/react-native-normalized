f39d4be64f0fcf72e83e58b54b227947d137dbc3
Jonny Burger <jonathanburger11@gmail.com>
2020-07-21 12:44:17 +0200

    refactor textinput for better type support

--- src/TextInput.tsx
+++ src/TextInput.tsx
@@ -1,25 +1,29 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, useMemo } from "react";
 import {
   TextInput as NativeTextInput,
   TextInputProps,
-  StyleSheet
+  StyleSheet,
 } from "react-native";
 
 const styles = StyleSheet.create({
   black: {
-    color: "black"
-  }
+    color: "black",
+  },
 });
 
-const TextInput = forwardRef((props: TextInputProps, ref: React.Ref<any>) => {
+const TextInput: React.RefForwardingComponent<
+  NativeTextInput,
+  TextInputProps
+> = forwardRef((props: TextInputProps, ref: React.Ref<NativeTextInput>) => {
   const newProps = props.multiline ? { textAlignVertical: "top" as "top" } : {};
   const { style, ...otherProps } = props;
+  const styleProp = useMemo(() => [styles.black, style].filter(Boolean), []);
   return (
     <NativeTextInput
       {...(ref ? { ref } : {})}
       underlineColorAndroid="transparent"
       placeholderTextColor="#C7C7CD"
-      style={[styles.black, style].filter(Boolean)}
+      style={styleProp}
       {...newProps}
       {...otherProps}
     />
